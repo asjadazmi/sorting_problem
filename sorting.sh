f3cd32911864a6233a54ff3eb88236467cea5681
asjadazmi <Asjadazmi0786@gmail.com>
2022-05-12 15:24:03 +0530

    solved computation

--- sorting.sh
+++ sorting.sh
@@ -6,4 +6,15 @@ read -p "enter the value of b: " b;
 read -p "enter the value of c: " c;
 
 expr[0]=$(($a+$b*$c));
-echo "the value of compute of 3 input is: ${expr[@]}";
+expr[1]=$(($a*$b+$c));
+expr[2]=$(($c+$a/$b));
+expr[3]=$(($a%$b+$c));
+
+
+
+echo "the value of compute of 3 input is: ${expr[0]}";
+echo "the value of compute of 3 input is: ${expr[1]}";
+echo "the value of compute of 3 input is: ${expr[2]}";
+echo "the value of compute of 3 input is: ${expr[3]}";
+
+echo "${expr[@]}"
