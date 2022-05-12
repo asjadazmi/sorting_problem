e336e2d6b7648d294a4904122b26badbc4335f01
asjadazmi <Asjadazmi0786@gmail.com>
2022-05-12 15:12:53 +0530

    value input

--- sorting.sh
+++ sorting.sh
@@ -4,3 +4,6 @@ echo "welcome on sorting problem";
 read -p "enter the value of a: " a;
 read -p "enter the value of b: " b;
 read -p "enter the value of c: " c;
+
+expr[0]=$(($a+$b*$c));
+echo "the value of compute of 3 input is: ${expr[@]}";
